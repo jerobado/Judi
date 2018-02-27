bc002c654a3e2e5d15089157056fe6519ce3c33d
Jero <tokidokitalkyou@gmail.com>
2018-02-27 21:14:52 +0800

    Adding a method that will determine if the app is running frozen or not

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@
     Author: Jero Bado
  """
 
+import os
 import sys
 from PyQt5.QtWidgets import QApplication
 from src.resources.constant import (__appname__,
@@ -15,8 +16,26 @@ APP = QApplication(sys.argv)
 APP.setOrganizationName(__orgname__)
 APP.setApplicationName(__appname__)
 
+
+def check_environment():
+    frozen = 'not'
+    if getattr(sys, 'frozen', False):
+        # we are running in a bundle
+        frozen = 'ever so'
+        bundle_dir = sys._MEIPASS
+    else:
+        # we are running in a normal Python environment
+        bundle_dir = os.path.dirname(os.path.abspath(__file__))
+    print('we are', frozen, 'frozen')
+    print('bundle dir is', bundle_dir)
+    print('sys.argv[0] is', sys.argv[0])
+    print('sys.executable is', sys.executable)
+    print('os.getcwd is', os.getcwd())
+
+
 if __name__ == '__main__':
     from src.gui.main.main_window import JudiWindow
+    check_environment()
     window = JudiWindow()
     window.clipboard = APP.clipboard()
     window.show()
